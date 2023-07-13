6ea35124d6eed82695eaf7e7023edcdb410ec2ad
Evelina Neumann <evelina.neumann1@gmail.com>
2023-07-13 14:42:18 +0200

    added carousel for pets

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require("./db");
 const express = require("express");
 
 const app = express();
+const cors = require('cors');
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
@@ -20,6 +21,8 @@ app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+app.use(cors());
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
