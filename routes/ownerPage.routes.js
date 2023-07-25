952e125b6941e94a1338f1ae59fb93088bff50e5
kharisma2806 <kiran.nankani28@gmail.com>
2023-07-25 20:19:15 +0200

    petprofile done

--- routes/ownerPage.routes.js
+++ routes/ownerPage.routes.js
@@ -7,21 +7,19 @@ const Pet = require("../models/Pet.model");
 const jwt = require("jsonwebtoken");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-
-
-
 //Owner Page Routes
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name:  'dnstseshn',
-  api_key:  '537231312596738',
-  api_secret:  '-pktoj2PPvkowDtf5NcnoakXdpI' ,
+  cloud_name: "dnstseshn",
+  api_key: "537231312596738",
+  api_secret: "-pktoj2PPvkowDtf5NcnoakXdpI",
 });
 
 // POST /pet/petprofile - Creates a new pet and associates it with the authenticated owner
 router.post("/petprofile", isAuthenticated, async (req, res, next) => {
   try {
-    const { name, category, type, age, temper, special_needs, image } = req.body;
+    const { name, category, type, age, temper, special_needs, image } =
+      req.body;
 
     // Get the authenticated owner ID from the request payload
     const ownerId = req.payload._id;
@@ -48,7 +46,6 @@ router.post("/petprofile", isAuthenticated, async (req, res, next) => {
   }
 });
 
-
 // Handle image upload
 router.post("/pet/upload", upload.single("file"), (req, res) => {
   const file = req.file;
@@ -63,5 +60,60 @@ router.post("/pet/upload", upload.single("file"), (req, res) => {
   });
 });
 
+// DELETE /pet/petprofile/:id - Deletes an existing pet profile
+router.delete("/pet/deletepet/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    const petId = req.params.id; // Get the pet ID from the request params
+    const ownerId = req.payload._id; // Get the authenticated owner ID from the request payload
+
+    // Check if the authenticated owner is the owner of the pet
+    const pet = await Pet.findOne({ _id: petId, owner: ownerId });
+    if (!pet) {
+      return res.status(404).json({ error: "Pet not found" });
+    }
+
+    // Remove the pet from the database
+    await Pet.findByIdAndRemove(petId);
+
+    // Send a response indicating successful deletion
+    res.json({ message: "Pet deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /pet/petprofile/:id - Updates an existing pet profile
+router.put("/pet/editpet/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    const petId = req.params.id; // Get the pet ID from the request params
+    const ownerId = req.payload._id; // Get the authenticated owner ID from the request payload
+
+    // Check if the authenticated owner is the owner of the pet
+    const pet = await Pet.findOne({ _id: petId, owner: ownerId });
+    if (!pet) {
+      return res.status(404).json({ error: "Pet not found" });
+    }
+
+    const { name, category, type, age, temper, special_needs, image } =
+      req.body;
+
+    // Update the pet profile with the new data
+    pet.name = name;
+    pet.category = category;
+    pet.type = type;
+    pet.age = age;
+    pet.temper = temper;
+    pet.special_needs = special_needs;
+    pet.image = image;
+
+    // Save the updated pet profile
+    await pet.save();
+
+    // Send a response with the updated pet object
+    res.json({ pet });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
